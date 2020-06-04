8eba5ce7f64647fb06748d26d59519bb25049ae9
afunx <afunx.bai@ubtrobot.com>
2020-06-04 20:43:03 +0800

    支持不同模块的字符串

--- translate/main.py
+++ translate/main.py
@@ -20,6 +20,18 @@ class Project:
     def __str__(self):
         return 'sheetName: %s, rootPath: %s' %(self.sheetName, self.rootPath)
 
+class Target:
+    # 目标文件路径
+    path = ""
+    # 目标文件模块
+    module = ""
+
+    def __init__(self, path, module):
+        self.path = path
+        self.module = module
+
+    def __str__(self):
+        return 'path: %s, module: %s' %(self.path, self.module)
 
 def createproject():
     projectlist = list()
@@ -41,13 +53,18 @@ def createsheets(workbook, projectlist):
         project.sheet.set_column('D:D', 50)
         project.sheet.set_column('E:E', 50)
         project.sheet.set_column('F:F', 50)
+        project.sheet.set_column('G:G', 50)
     return
 
 def printboundary(str):
     print("=======================================" + str + "=======================================")
     return
 
-def fillsheet(sheet, file, iddict, column, modify):
+def fillsheet(project, target, iddict, column, modify):
+    file = target.path
+    module = target.module
+
+    sheet = project.sheet
     tree = ET.parse(file)
     root = tree.getroot()
 
@@ -58,7 +75,7 @@ def fillsheet(sheet, file, iddict, column, modify):
         if modify:
             if child.tag == "string":
                 line = line + 1
-                textid = child.attrib["name"]
+                textid = module + "$" + child.attrib["name"]
                 text = child.text
                 iddict[textid] = line
                 sheet.write(line, 0, textid)
@@ -68,7 +85,7 @@ def fillsheet(sheet, file, iddict, column, modify):
                 childlist = list(child)
                 for item in childlist:
                     line = line + 1
-                    textid = child.attrib["name"] + "$string-array$" + str(childlist.index(item))
+                    textid = module + "$" + child.attrib["name"] + "$string-array$" + str(childlist.index(item))
                     text = item.text
                     iddict[textid] = line
                     sheet.write(line, 0, textid)
@@ -78,7 +95,7 @@ def fillsheet(sheet, file, iddict, column, modify):
                 childlist = list(child)
                 for item in childlist:
                     line = line + 1
-                    textid = child.attrib["name"] + "$plurals$" + item.attrib["quantity"]
+                    textid = module + "$" + child.attrib["name"] + "$plurals$" + item.attrib["quantity"]
                     text = item.text
                     iddict[textid] = line
                     sheet.write(line, 0, textid)
@@ -86,7 +103,7 @@ def fillsheet(sheet, file, iddict, column, modify):
 
         else:
             if child.tag == "string":
-                textid = child.attrib["name"]
+                textid = module + "$" + child.attrib["name"]
                 text = child.text
                 line = iddict.get(textid)
                 sheet.write(line, column, text)
@@ -94,7 +111,7 @@ def fillsheet(sheet, file, iddict, column, modify):
             elif child.tag == "string-array":
                 childlist = list(child)
                 for item in childlist:
-                    textid = child.attrib["name"] + "$string-array$" + str(childlist.index(item))
+                    textid = module + "$" + child.attrib["name"] + "$string-array$" + str(childlist.index(item))
                     text = item.text
                     line = iddict.get(textid)
                     sheet.write(line, column, text)
@@ -102,7 +119,7 @@ def fillsheet(sheet, file, iddict, column, modify):
             elif child.tag == "plurals":
                 childlist = list(child)
                 for item in childlist:
-                    textid = child.attrib["name"] + "$plurals$" + item.attrib["quantity"]
+                    textid = module + "$" + child.attrib["name"] + "$plurals$" + item.attrib["quantity"]
                     text = item.text
                     line = iddict.get(textid)
                     sheet.write(line, column, text)
@@ -117,8 +134,9 @@ def generatesheet(project):
     valueslist.append("values-zh-rTW")
     valueslist.append("values-th-rTH")
     valueslist.append("values-ko-rKR")
-    # 文件列表的列表, fileslist[0]为values文件下的全部文件, fileslist[1]为values-en文件夹下的全部文件
-    fileslist = list()
+    # Target列表的列表, targetlist[0]为values文件下的全部Target, targetlist[1]为values-en文件夹下的全部Target
+    # Target的path为文件路径, Target的module为模块
+    targetlist = list()
     # 行
     i = 0
     # 列
@@ -126,7 +144,7 @@ def generatesheet(project):
     # 第一行写列名
     project.sheet.write(i, j, "id")
     for values in valueslist:
-        fileslist.append(list())
+        targetlist.append(list())
         j = j + 1
         project.sheet.write(i, j, values)
 
@@ -135,21 +153,22 @@ def generatesheet(project):
     for root, dirs, files in os.walk(project.rootPath):
         for file in files:
             if file == "strings.xml" or file == "strings_untranslated.xml":
-                target = (root + "/" + file).replace("/", "\\")
-                targetsplit = target.split("\\")
-                index = valueslist.index(targetsplit[-2])
-                fileslist[index].append(target)
-                print("generatesheet() 目标文件: " + target + ", index: " + str(index))
+                path = (root + "/" + file).replace("/", "\\")
+                pathsplit = path.split("\\")
+                index = valueslist.index(pathsplit[-2])
+                module = pathsplit[-6]
+                targetlist[index].append(Target(path, module))
+                print("generatesheet() 目标文件: " + path + ", index: " + str(index) + ", module: " + module)
 
     # id索引
     iddict = dict()
     # 处理values
-    for files in fileslist:
-        column = fileslist.index(files) + 1
-        for file in files:
+    for targets in targetlist:
+        column = targetlist.index(targets) + 1
+        for target in targets:
             # 只有strings可以修改iddict
             modify = column == 1
-            fillsheet(project.sheet, file, iddict, column, modify)
+            fillsheet(project, target, iddict, column, modify)
     return
 
 def main():
